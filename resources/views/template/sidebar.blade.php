59c7f5956a56fd4faa2a914bf31f474e335406e5
Phinkurniawan <2072035@maranatha.ac.id>
2022-10-24 23:19:22 +0700

    commit

--- resources/views/template/sidebar.blade.php
+++ resources/views/template/sidebar.blade.php
@@ -6,10 +6,10 @@
     <div class="offcanvas-body">
         <ul class="navbar-nav">
             <li class="nav-item">
-              <a class="nav-link" href="#">Home</a>
+              <a class="nav-link" href="#"><i   class="fa-solid fa-house" style="font-size: 30px"></i>Home</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="#">News / Event</a>
+              <a class="nav-link" href="#"><i class="fa-regular fa-newspaper"></i>News / Event</a>
             </li>
           </ul>
     </div>
