610472b46baf97aff79d6eaef9d452037a1764bb
Phinkurniawan <2072035@maranatha.ac.id>
2022-10-25 19:34:32 +0700

    add ProductsController and NewsController in Controllers

---
+++ app/Http/Controllers/NewsController.php
@@ -0,0 +1,29 @@
+<?php
+namespace App\Http\Controllers;
+
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Routing\Controller as BaseController;
+
+class NewsController extends Controller
+{
+    public function index() {
+
+    }
+    public function showAllNews($kategori){
+
+    }
+    public function showNewsDetail(){
+
+    }
+    public function insert(){
+
+    }
+    public function updateNews($id){
+
+    }
+    public function delete($Id){
+
+    }
+}
---
+++ app/Http/Controllers/ProductsController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class ProductsController
+{
+    public function index() {
+
+    }
+    public function showAllProducts($id){
+
+    }
+    public function showProductsDetail($id){
+
+    }
+    public function insertProduct(){
+
+    }
+    public function updateProducts($id){
+
+    }
+    public function delete($Id){
+
+    }
+}
